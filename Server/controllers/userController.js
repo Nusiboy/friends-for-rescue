ed806468dbfb685913782a4bb8d2098132a8b290
Nusiboy <shahar.nusboim@gmail.com>
2023-07-24 13:04:52 +0300

    users-working

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require("../modles/userModel");
+const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 exports.users = (req, res) => {

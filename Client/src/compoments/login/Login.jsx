2146eae42c2a7d4307ddbf48a889043a8614b37b
Nusiboy <shahar.nusboim@gmail.com>
2023-08-08 10:30:03 +0300

    css changes

--- Client/src/compoments/login/Login.jsx
+++ Client/src/compoments/login/Login.jsx
@@ -1,25 +1,15 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import "../login/Login.css";
 import { useNavigate } from "react-router-dom";
-import Register from "../register/Register";
-// const [emailValue, setEmailValue] = useState("");
 import { RefContext } from "../../context/RefreshConmtext";
 
 function Login() {
   const [userNameValue, setUserNameValue] = useState("");
   const [passwordValue, setPasswordValue] = useState("");
-  // const [refresh, setRefresh] = useState();
   const { ref, setRef } = useContext(RefContext);
-  // const [user, setUser] = useState([]);
   const navigate = useNavigate();
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost:3001/users")
-  //     .then(({ data }) => setUser(data))
-  //     .catch((err) => console.log(err.message));
-  // }, [refresh]);
   function loginUser() {
     axios
       .post("http://localhost:3000/users/login", {
@@ -32,7 +22,6 @@ function Login() {
         localStorage.setItem("type", user.data.type);
         localStorage.setItem("usernum", user.data.num);
         localStorage.setItem("username", user.data.userName);
-  
         setRef(!ref);
         navigate("/GoogleMaps");
       })
@@ -71,7 +60,7 @@ function Login() {
         </button>
       </div>
       <div>
-        <Link to={"/Register"}>
+        <Link id="link-to" to={"/Register"}>
           Haven't signed to our services? click to sign! 
         </Link>
       </div>

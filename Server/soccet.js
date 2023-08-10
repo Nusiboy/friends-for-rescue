c8641af48d45b208cdaa5807d853d88346305e82
Nusiboy <shahar.nusboim@gmail.com>
2023-08-10 09:12:14 +0300

    rendering routers

--- Server/soccet.js
+++ Server/soccet.js
@@ -24,34 +24,36 @@
 // http.listen(port, () => {
 //   console.log(`Server listening on port ${port}`);
 // });
-const express=require("express")
-const app=express()
-const port=4000
-const http=require("http").Server(app)
-const cors= require("cors")
+const express = require("express");
+const app = express();
+const port = 4000;
+const http = require("http").Server(app);
+const cors = require("cors");
 
 app.use(cors());
 
-const socketIO=require("socket.io")(http,{
-    cors:{
-        origin:"*",
-    },
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  },
 });
 
-socketIO.on("connection",(socket)=>{
-    console.log("user connected")
-    socketIO.emit("userconnected",`${socket.id}`)
+socketIO.on("connection", (socket) => {
+  console.log("user connected");
+  socketIO.emit("userconnected", `${socket.id}`);
 
-    socket.on("message",(data)=>{
-        socketIO.emit("messageResponse",data);
-    });
-    socket.on("disconnect",()=>{
-        console.log("user disconnected");
-        socketIO.emit("userDisconnected",`${socket.id}`);
-    })
-})
+  socket.on("message", (data) => {
+    socketIO.emit("messageResponse", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    socketIO.emit("userDisconnected", `${socket.id}`);
+  });
+});
 
-
-http.listen(port,()=>{
-    console.log(`server listening on ${port}`);
-})
+app.get("/", (req, res) => {
+  res.status(200).send("hello world");
+});
+http.listen(port, () => {
+  console.log(`server listening on ${port}`);
+});
